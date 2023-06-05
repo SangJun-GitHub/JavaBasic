c63075b2bf91f7e332c4dc3e19563bdacc0c4dd5
Sang Jun Park <sjpark24@myseneca.ca>
2023-06-04 23:40:40 -0400

    commit

---
+++ Ch8/ExceptionEx6.java
@@ -0,0 +1,20 @@
+package Ch8;
+
+/**
+ * Created by Sang Jun Park on 6/4/2023.
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class ExceptionEx6 {
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.println(2);
+        try{
+            System.out.println(3);
+            System.out.println(0/0);
+            System.out.println(4);
+        }catch(Exception e){
+            System.out.println(5);
+        }
+        System.out.println(6);
+    }
+}
