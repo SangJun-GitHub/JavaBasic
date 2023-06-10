3d8a6d49394c41a2822bcc98e6b66206159f0fae
Sang Jun Park <sjpark24@myseneca.ca>
2023-06-10 00:46:47 -0400

    commit

---
+++ Ch9/RandomEx1.java
@@ -0,0 +1,23 @@
+package Ch9;
+
+import java.util.Random;
+
+/**
+ * Created by Sang Jun Park on 6/9/2023.
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class RandomEx1 {
+    public static void main(String[] args) {
+        Random rand  = new Random(1);
+        Random rand2 = new Random(1);
+
+        System.out.println(" = rand = ");
+        for(int i = 0; i < 5; i ++)
+            System.out.println(i + ":" + rand.nextInt());
+
+        System.out.println();
+        System.out.println(" = rand2 = ");
+        for(int i = 0; i < 5; i ++)
+            System.out.println(i + ":" + rand2.nextInt());
+    }
+}
---
+++ Ch9/RandomEx4.java
@@ -0,0 +1,36 @@
+package Ch9;
+
+/**
+ * Created by Sang Jun Park on 6/9/2023.
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class RandomEx4 {
+    final static int RECODE_NUM = 10;
+    final static String TABLE_NAME = "TEST_TABLE";
+    final static String[] CODE1 = {"010","011","017","018","019"};
+    final static String[] CODE2 = {"Male", "Female"};
+    final static String[] CODE3 = {"10's","20's","30's","40's","50's"};
+
+    public static void main(String[] args) {
+        for(int i = 0; i < RECODE_NUM; i++){
+            System.out.println(" INSERT INTO " + TABLE_NAME
+                                + " VALUES ("
+                                + "'" + getRandArr(CODE1) + "'"
+                                + ",'" + getRandArr(CODE2) + "'"
+                                + ",'" + getRandArr(CODE3) + "'"
+                                + ", " + getRand(100, 200) + "'"
+                                + "); ");
+
+        }
+    }
+    public static String getRandArr(String[] arr){
+        return arr[getRand(arr.length -1)];
+    }
+
+    public static int getRand(int n){
+        return getRand(0, n);
+    }
+    public static int getRand(int from, int to){
+        return (int)(Math.random() * (Math.abs(to - from)+1)) + Math.min(from, to);
+    }
+}
