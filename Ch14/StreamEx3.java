644d2608376deb2141a7aa4f2142a51c22bfe874
Sang Jun Park <sjpark24@myseneca.ca>
2023-07-14 23:01:24 -0400

    commit

--- Ch14/StreamEx3.java
+++ Ch14/StreamEx3.java
@@ -1,11 +1,43 @@
 package Ch14;
 
+import java.util.Comparator;
+import java.util.IntSummaryStatistics;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Created by Sang Jun Park on 7/12/2023.
  * Github : http://github.com/SangJun-GitHub
  */
 public class StreamEx3 {
     public static void main(String[] args) {
+        Student2[] stuArr = {
+                new Student2("AAA", 3, 300),
+                new Student2("BBB", 1, 200),
+                new Student2("CCC", 2, 100),
+                new Student2("DDD", 2, 150),
+                new Student2("EEE", 1, 200),
+                new Student2("FFF", 3, 290),
+                new Student2("GGG", 3, 180)
+        };
+
+        Stream<Student2> stuStream = Stream.of(stuArr);
+
+
+        stuStream.sorted(Comparator.comparing(Student2::getClassNum)
+                .thenComparing(Comparator.naturalOrder()))
+                .forEach(System.out::println);
+
+
+        stuStream = Stream.of(stuArr);
+        IntStream stuScoreStream = stuStream.mapToInt(Student2::getTotalScore);
+        IntSummaryStatistics stat = stuScoreStream.summaryStatistics();
+
+        System.out.println("count       :   " + stat.getCount());
+        System.out.println("sum         :   " + stat.getSum());
+        System.out.println("average     :   " + stat.getAverage());
+        System.out.println("min         :   " + stat.getMin());
+        System.out.println("max         :   " + stat.getMax());
 
     }
 }
