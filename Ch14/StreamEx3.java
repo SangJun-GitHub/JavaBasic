da81d7dd43559d9ab5f23e08481671f1709e8de8
Sang Jun Park <sjpark24@myseneca.ca>
2023-07-14 22:54:28 -0400

    commit

--- Ch14/StreamEx3.java
+++ Ch14/StreamEx3.java
@@ -5,4 +5,34 @@ package Ch14;
  * Github : http://github.com/SangJun-GitHub
  */
 public class StreamEx3 {
+    public static void main(String[] args) {
+
+    }
+}
+class Student2 implements Comparable<Student2>{
+    String name;
+    int classNum;
+    int totalScore;
+
+    Student2(String name, int classNum, int totalScore){
+        this.name = name;
+        this.classNum = classNum;
+        this.totalScore = totalScore;
+    }
+
+    public String toString(){
+        return String.format("[%s, %d, %d]", name, classNum, totalScore).toString();
+    }
+    String getName(){
+        return name;
+    }
+    int getClassNum(){
+        return classNum;
+    }
+    int getTotalScore(){
+        return totalScore;
+    }
+    public int compareTo(Student2 s){
+        return s.totalScore - this.totalScore;
+    }
 }
