3bb69d27182287ec188df1148926fad98fa0be34
Sang Jun Park <sjpark24@myseneca.ca>
2023-07-18 23:29:28 -0400

    commit

--- Ch14/StreamEx5.java
+++ Ch14/StreamEx5.java
@@ -1,8 +1,33 @@
 package Ch14;
 
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 /**
  * Created by Sang Jun Park on 7/15/2023.
  * Github : http://github.com/SangJun-GitHub
  */
 public class StreamEx5 {
+    public static void main(String[] args) {
+        String[] strArr = {
+                "Inheritance", "Java", "Lambda", "stream",
+                "OptionalDouble", "IntStream", "count", "sum"
+        };
+
+        Stream.of(strArr).forEach(System.out::println);
+
+        boolean noEmptyStr = Stream.of(strArr).noneMatch(s -> s.length() == 0);
+        Optional<String> sWord = Stream.of(strArr).filter(s -> s.charAt(0) == 's').findFirst();
+
+        System.out.println("noEmptyStr  = " + noEmptyStr);
+        System.out.println("sWord       = " + sWord);
+
+        IntStream intStream1 = Stream.of(strArr).mapToInt(String::length);
+        IntStream intStream2 = Stream.of(strArr).mapToInt(String::length);
+        IntStream intStream3 = Stream.of(strArr).mapToInt(String::length);
+        IntStream intStream4 = Stream.of(strArr).mapToInt(String::length);
+
+        int count = intStream1.reduce(0, (a,b) -> a + 1);
+    }
 }
