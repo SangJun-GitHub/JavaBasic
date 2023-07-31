02e1ff1f029800e5ecd68f1102e279722e9c0a37
Sang Jun Park <sjpark24@myseneca.ca>
2023-07-31 07:11:50 -0400

    commit

--- Ch15/FileViewer.java
+++ Ch15/FileViewer.java
@@ -1,8 +1,21 @@
 package Ch15;
 
+import java.io.FileInputStream;
+
 /**
  * Created by Sang Jun Park on 7/30/2023.
  * Github : http://github.com/SangJun-GitHub
  */
+
+//java FileViewer.java FileViewer.java
 public class FileViewer {
+    public static void main(String[] args) throws Exception {
+        FileInputStream fis = new FileInputStream(args[0]);
+        int data = 0;
+
+        while((data = fis.read()) != -1){
+            char c = (char) data;
+            System.out.print(c);
+        }
+    }
 }
