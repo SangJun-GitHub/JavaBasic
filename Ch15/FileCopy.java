44fe842228cf856ee169a2288c821843f3db78a1
Sang Jun Park <sjpark24@myseneca.ca>
2023-07-30 21:41:40 -0400

    commit

---
+++ Ch15/FileCopy.java
@@ -0,0 +1,8 @@
+package Ch15;
+
+/**
+ * Created by Sang Jun Park on 7/30/2023.
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class FileCopy {
+}
--- Ch15/FileViewer.java
+++ Ch15/FileViewer.java
@@ -4,5 +4,5 @@ package Ch15;
  * Created by Sang Jun Park on 7/30/2023.
  * Github : http://github.com/SangJun-GitHub
  */
-public class FileViwer {
+public class FileViewer {
 }
