7be26ffff188585b9d4a47a81d1fe2b2fc5d92a7
Sang Jun Park <sjpark24@myseneca.ca>
2023-08-17 23:16:41 -0400

    commit

---
+++ Ch15/SerialEx2.java
@@ -0,0 +1,35 @@
+package Ch15;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+
+/**
+ * Created by Sang Jun Park on 8/17/2023.
+ * Github : http://github.com/SangJun-GitHub
+ */
+public class SerialEx2 {
+    public static void main(String[] args) {
+        try{
+            String fileName = "src\\main\\resources\\UserInfo.ser";
+            FileInputStream fis = new FileInputStream(fileName);
+            BufferedInputStream bis = new BufferedInputStream(fis);
+
+            ObjectInputStream in = new ObjectInputStream(bis);
+
+            //Must match input oder with output oder
+            UserInfo u1 = (UserInfo) in.readObject();
+            UserInfo u2 = (UserInfo) in.readObject();
+            ArrayList<UserInfo> list = (ArrayList<UserInfo>) in.readObject();
+
+            System.out.println(u1);
+            System.out.println(u2);
+            System.out.println(list);
+            in.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
