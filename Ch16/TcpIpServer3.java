799b27ab609d24645ac89076924582d623ac27c7
Sang Jun Park <sjpark24@myseneca.ca>
2023-08-27 23:25:17 -0400

    commit

--- Ch16/TcpIpServer3.java
+++ Ch16/TcpIpServer3.java
@@ -1,8 +1,57 @@
 package Ch16;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Sang Jun Park on 8/25/2023.
  * Github : http://github.com/SangJun-GitHub
  */
 public class TcpIpServer3 {
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+
+        try{
+            serverSocket = new ServerSocket(7777);
+            System.out.println(getTime() + " sever is ready.");
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        while(true){
+            try{
+                System.out.println(getTime() + " : Waiting request.");
+
+                serverSocket.setSoTimeout(5*1000);
+                Socket socket = serverSocket.accept();
+                System.out.println(getTime() + " : got a request from " + socket.getInetAddress() + ".");
+
+                OutputStream out = socket.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(out);
+
+                dos.writeUTF("[Notice] Test Message1 from Server.");
+                System.out.println(getTime() + " : sent data");
+
+                dos.close();
+                socket.close();
+            } catch (SocketTimeoutException e){
+                System.out.println("Connection time over. Exit program.");
+                System.exit(0);
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static String getTime(){
+        SimpleDateFormat f = new SimpleDateFormat("[hh:mm:ss]");
+        return f.format(new Date());
+    }
 }
