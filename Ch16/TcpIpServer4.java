684ded91e63f6a85ad76b42dd644362e4a8d0402
Sang Jun Park <sjpark24@myseneca.ca>
2023-08-28 23:29:48 -0400

    commit

--- Ch16/TcpIpServer4.java
+++ Ch16/TcpIpServer4.java
@@ -1,8 +1,70 @@
 package Ch16;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Sang Jun Park on 8/28/2023.
  * Github : http://github.com/SangJun-GitHub
  */
-public class TcpIpServer4 {
+public class TcpIpServer4 implements Runnable {
+    ServerSocket serverSocket = null;
+    Thread[] threads;
+
+    public static void main(String[] args) {
+        TcpIpServer4 server = new TcpIpServer4(5);
+        server.start();
+    }
+
+    public TcpIpServer4(int num) {
+        try {
+            serverSocket = new ServerSocket(7777);
+            System.out.println(getTime() + " sever is ready.");
+
+            threads = new Thread[num];
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start() {
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(this);
+            threads[i].start();
+        }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                System.out.println(getTime() + " : Waiting request.");
+
+                Socket socket = serverSocket.accept();
+                System.out.println(getTime() + " : got a request from " + socket.getInetAddress() + ".");
+
+                OutputStream out = socket.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(out);
+
+                dos.writeUTF("[Notice] Test Message1 from Server.");
+                System.out.println(getTime() + " : sent data");
+
+                dos.close();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static String getTime(){
+        String name = Thread.currentThread().getName();
+        SimpleDateFormat f = new SimpleDateFormat("[hh:mm:ss]");
+        return f.format(new Date()) + name;
+    }
 }
+
